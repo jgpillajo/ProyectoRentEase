6228d71d08b5cd8a8486ac895d0c26d1c7f6013e
Carlos Trujillo <carlos.trujillo.bazante270494@gmail.com>
2024-06-19 19:31:50 -0500

    save

--- javascript/home.js
+++ javascript/home.js
@@ -1,8 +1,48 @@
 let flatStorage = [
-  { provincia: 'Pichincha', ciudad: 'Zapotillo', calle: 'Eloy alfaro', numeracion: 'n34-118', area: 125, aire: true, construcion: 1994, precio: 1200, disponibilidad: '2023/8/12' },
-  { provincia: 'Pichincha', ciudad: 'Azcabulli', calle: 'Eloy alfaro2', numeracion: 'n34-118', area: 88, aire: false, construcion: 1996, precio: 450, disponibilidad: '2023/8/12' },
-  { provincia: 'Pichincha', ciudad: 'Farraon', calle: 'Eloy alfaro3', numeracion: 'n34-118', area: 69, aire: true, construcion: 1998, precio: 400, disponibilidad: '2023/8/12' },
-  { provincia: 'Pichincha', ciudad: 'Quito', calle: 'Eloy alfaro4', numeracion: 'n34-118', area: 78, aire: false, construcion: 2000, precio: 500, disponibilidad: '2023/8/12' },
+  {
+    provincia: "Pichincha",
+    ciudad: "Zapotillo",
+    calle: "Eloy alfaro",
+    numeracion: "n34-118",
+    area: 125,
+    aire: true,
+    construcion: 1994,
+    precio: 1200,
+    disponibilidad: "2023/8/12",
+  },
+  {
+    provincia: "Pichincha",
+    ciudad: "Azcabulli",
+    calle: "Eloy alfaro2",
+    numeracion: "n34-118",
+    area: 88,
+    aire: false,
+    construcion: 1996,
+    precio: 450,
+    disponibilidad: "2023/8/12",
+  },
+  {
+    provincia: "Pichincha",
+    ciudad: "Farraon",
+    calle: "Eloy alfaro3",
+    numeracion: "n34-118",
+    area: 69,
+    aire: true,
+    construcion: 1998,
+    precio: 400,
+    disponibilidad: "2023/8/12",
+  },
+  {
+    provincia: "Pichincha",
+    ciudad: "Quito",
+    calle: "Eloy alfaro4",
+    numeracion: "n34-118",
+    area: 78,
+    aire: false,
+    construcion: 2000,
+    precio: 500,
+    disponibilidad: "2023/8/12",
+  },
 ];
 
 const pushImg = () => {
@@ -13,67 +53,67 @@ const pushImg = () => {
 
 pushImg();
 
-getvalue = t => {
-  if (t.value === 'Ciudad') {
-    console.log('getvalue ciudad');
+getvalue = (t) => {
+  if (t.value === "Ciudad") {
+    console.log("getvalue ciudad");
     creadorFlats(OrdenCiudad(flatStorage));
-  } else if (t.value === 'Precio') {
-    console.log('getvalue precio');
+  } else if (t.value === "Precio") {
+    console.log("getvalue precio");
     creadorFlats(OrdenPrecio(flatStorage));
-  } else if (t.value === 'Área') {
-    console.log('getvalue area');
+  } else if (t.value === "Área") {
+    console.log("getvalue area");
     creadorFlats(OrdenArea(flatStorage));
   }
 };
 
-const OrdenCiudad = flatStorage => {
+const OrdenCiudad = (flatStorage) => {
   return flatStorage.sort((a, b) => a.ciudad.localeCompare(b.ciudad));
 };
-const OrdenPrecio = flatStorage => {
+const OrdenPrecio = (flatStorage) => {
   return flatStorage.sort((a, b) => a.precio - b.precio);
 };
 
-const OrdenArea = flatStorage => {
+const OrdenArea = (flatStorage) => {
   return flatStorage.sort((a, b) => a.area - b.area);
 };
 
-const ordenPrecio2 = flatStorage => {
+const ordenPrecio2 = (flatStorage) => {
   return flatStorage.sort((a, b) => b.precio - a.precio);
 };
 
-const OrdenArea2 = flatStorage => {
+const OrdenArea2 = (flatStorage) => {
   return flatStorage.sort((a, b) => b.area - a.area);
 };
 
 const removerTodosChildren = () => {
-  let remove = document.getElementById('grid-flats');
+  let remove = document.getElementById("grid-flats");
   remove.replaceChildren();
 };
 
-const creadorFlats = ordenarBoton => {
+const creadorFlats = (ordenarBoton) => {
   if (ordenarBoton === false) {
     removerTodosChildren();
-    let divGrid2 = document.getElementById('grid-flats');
-    let temp = document.createElement('div');
-    temp.classList.add('grid-flat-solo');
+    let divGrid2 = document.getElementById("grid-flats");
+    let temp = document.createElement("div");
+    temp.classList.add("grid-flat-solo");
     temp.innerHTML = `
     <img class="img-flat" src="/img/no_img.jpg" alt="">
     <div class = "contenedor-flat">
     <p class="avenida-flat">No hay información disponible</p>
-    <p><span class = "precio">No hay infomración disponible </span> $/mes</p>
-    <p>No hay infomración disponible m², aire acondicionado No hay infomración disponible</p>
-    <p>Año de construción: No hay infomración disponible</p>
-    <p>Disponible desde: No hay infomración disponible</p>
+    <p><span class = "precio">No hay infomración disponible </span></p>
+    <p>No hay infomración disponible</p>
+    <p>No hay infomración disponible</p>
+    <p>No hay infomración disponible</p>
     </div>`;
     divGrid2.appendChild(temp);
     return;
   }
   if (!ordenarBoton) {
     removerTodosChildren();
-    let divGrid2 = document.getElementById('grid-flats');
+    let divGrid2 = document.getElementById("grid-flats");
     for (let i = 0; i < flatStorage.length; i++) {
-      let temp = document.createElement('div');
-      temp.classList.add('grid-flat-solo');
+      let temp = document.createElement("div");
+      temp.classList.add("grid-flat-solo");
       temp.innerHTML = `
     <img class="img-flat" src="/img/flat${flatStorage[i].img}.webp" alt="">
     <div class = "contenedor-flat">
@@ -88,10 +128,10 @@ const creadorFlats = ordenarBoton => {
     return;
   }
   removerTodosChildren();
-  let divGrid = document.getElementById('grid-flats');
+  let divGrid = document.getElementById("grid-flats");
   for (let i = 0; i < ordenarBoton.length; i++) {
-    let temp = document.createElement('div');
-    temp.classList.add('grid-flat-solo');
+    let temp = document.createElement("div");
+    temp.classList.add("grid-flat-solo");
     temp.innerHTML = `
     <img class="img-flat" src="/img/flat${ordenarBoton[i].img}.webp" alt="">
     <div class = "contenedor-flat">
@@ -112,9 +152,9 @@ const datalistCiudades = () => {
   for (let [index, value] of flatOrdenados.entries()) {
     ciudades.push(`${value.ciudad}`);
   }
-  let ciudad = document.getElementById('ciudades');
+  let ciudad = document.getElementById("ciudades");
   for (let [index, value] of ciudades.entries()) {
-    let option = document.createElement('option');
+    let option = document.createElement("option");
     option.value = `${ciudades[index]}`;
     ciudad.appendChild(option);
   }
@@ -123,8 +163,8 @@ const datalistCiudades = () => {
 datalistCiudades();
 
 const precioHolder = () => {
-  let preciMin = document.getElementById('precio-min');
-  let preciMax = document.getElementById('precio-max');
+  let preciMin = document.getElementById("precio-min");
+  let preciMax = document.getElementById("precio-max");
   let temp = OrdenPrecio(flatStorage);
   preciMin.placeholder = `Valor mínimo - ${temp[0].precio}`;
   temp = ordenPrecio2(flatStorage);
@@ -133,8 +173,8 @@ const precioHolder = () => {
 precioHolder();
 
 const areaHolder = () => {
-  let preciMin = document.getElementById('area-min');
-  let preciMax = document.getElementById('area-max');
+  let preciMin = document.getElementById("area-min");
+  let preciMax = document.getElementById("area-max");
   let temp = OrdenArea(flatStorage);
   preciMin.placeholder = `Valor mínimo - ${temp[0].area}`;
   temp = OrdenArea2(flatStorage);
@@ -143,11 +183,11 @@ const areaHolder = () => {
 areaHolder();
 
 const filter = () => {
-  let precioMin = document.getElementById('precio-min').value;
-  let precioMax = document.getElementById('precio-max').value;
-  let areaMin = document.getElementById('area-min').value;
-  let areaMax = document.getElementById('area-max').value;
-  let ciudades = document.getElementsByName('ciudades')[0].value;
+  let precioMin = document.getElementById("precio-min").value;
+  let precioMax = document.getElementById("precio-max").value;
+  let areaMin = document.getElementById("area-min").value;
+  let areaMax = document.getElementById("area-max").value;
+  let ciudades = document.getElementsByName("ciudades")[0].value;
 
   if (precioMin <= 0 || precioMax <= 0 || areaMin <= 0 || areaMax <= 0) {
     let noFlat = false;
@@ -165,8 +205,8 @@ const filter = () => {
 };
 
 const filtro1 = (value, array) => {
-  if (value !== '') {
-    let filtro = array.filter(e => e.precio >= value);
+  if (value !== "") {
+    let filtro = array.filter((e) => e.precio >= value);
     return filtro;
   } else {
     return array;
@@ -174,32 +214,32 @@ const filtro1 = (value, array) => {
 };
 
 const filtro2 = (value, array) => {
-  if (value !== '') {
-    let filtro = array.filter(e => e.precio <= value);
+  if (value !== "") {
+    let filtro = array.filter((e) => e.precio <= value);
     return filtro;
   } else {
     return array;
   }
 };
 const filtro3 = (value, array) => {
-  if (value !== '') {
-    let filtro = array.filter(e => e.area >= value);
+  if (value !== "") {
+    let filtro = array.filter((e) => e.area >= value);
     return filtro;
   } else {
     return array;
   }
 };
 const filtro4 = (value, array) => {
-  if (value !== '') {
-    let filtro = array.filter(e => e.area <= value);
+  if (value !== "") {
+    let filtro = array.filter((e) => e.area <= value);
     return filtro;
   } else {
     return array;
   }
 };
 const filtro5 = (value, array) => {
-  if (value !== '') {
-    let filtro = array.filter(e => e.ciudad == value);
+  if (value !== "") {
+    let filtro = array.filter((e) => e.ciudad == value);
     return filtro;
   } else {
     return array;
